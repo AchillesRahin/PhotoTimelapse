85e83797411066412dbaf3bd6e7b0c3e7d48f413
Fredrick Esposito <fredrickesposito@gmail.com>
2021-04-16 15:42:26 -0400

    Adjust Android top padding

--- components/GalleryListSelect.js
+++ components/GalleryListSelect.js
@@ -88,7 +88,7 @@ const styles = StyleSheet.create({
   backgroundView: {
     marginTop: 10,
     flex: 1,
-    paddingTop: Platform.OS === 'android' ? 25 : 0
+    paddingTop: Platform.OS === 'android' ? 35 : 0
   },
   gridView: {
     marginTop: 10,
