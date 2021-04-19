9cf242407987f6abb2943b36565282db1dbf09c1
Fredrick Esposito <fredrickesposito@gmail.com>
2021-04-19 13:00:23 -0400

    Refactor Gallery Preview

--- components/GalleryPreview.js
+++ components/GalleryPreview.js
@@ -2,24 +2,14 @@ import React from 'react';
 import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
 
 const GalleryPreview = (props) => {
-    return props.gallery.imageList.length == 0 ? <PreviewWithoutThumbnail {...props} /> : <PreviewWithThumbnail {...props} />
+    return <GalleryThumbnail {...props} />
 }
 
-function PreviewWithThumbnail(props) {
+function GalleryThumbnail(props) {
     return (
         <View>
-            <TouchableOpacity onPress={props.onPress}>
-                <Image source={{uri: props.gallery.imageList[0].image}} resizeMode='cover' style={styles.galleryThumbnail}/>
-                <Text style={styles.galleryName}>{props.gallery.name}</Text>
-            </TouchableOpacity>
-        </View>
-    );
-}
-
-function PreviewWithoutThumbnail(props) {
-    return (
-        <View>
-            <TouchableOpacity style={styles.galleryThumbnail} onPress={props.onPress}>
+            <TouchableOpacity onPress={props.onPress} style={styles.galleryThumbnail}>
+                {props.gallery.imageList.length > 0 && <Image source={{uri: props.gallery.imageList[0].image}} resizeMode='cover' style={styles.galleryThumbnail}/>}
                 <Text style={styles.galleryName}>{props.gallery.name}</Text>
             </TouchableOpacity>
         </View>
